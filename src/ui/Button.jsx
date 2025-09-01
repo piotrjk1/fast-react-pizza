4f0f105981706ad3a3215fa6b57be9f3cb671001
Piotr Krzyzak <piotrjakubkrzyzak@gmail.com>
2025-09-01 13:44:42 +0200

    Update

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,10 +1,28 @@
-function Button({ children, disabled }) {
+import { Link } from "react-router-dom";
+
+function Button({ children, disabled, to, type }) {
+  const className =
+    "active:bg-yellow-400 text-sm inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+
+  const base =
+    "active:bg-yellow-400 inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      " px-4 text-sm py-2.5 md:px-6 md:py-3.5 active:bg-yellow-400 inline-block rounded-full border-2 border-stone-300 px-4 py-3 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 focus:ring-stone-200 hover:text-stone-800 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4",
+  };
+  if (to) {
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <button
-      disabled={disabled}
-      className="active:bg-state-400 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-    >
-      {disabled ? "Placing order..." : "Order now"}
+    <button disabled={disabled} className={styles[type]}>
+      {children}
     </button>
   );
 }
